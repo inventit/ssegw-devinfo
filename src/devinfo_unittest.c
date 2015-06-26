55d34b7ddb2004bfcfaebf7c860eb9a77432eada
Satoshi Hasegawa <shasegawa@yourinventit.com>
2015-06-26 13:23:04 +0900

    add test case for checking all items.

--- src/devinfo_unittest.c
+++ src/devinfo_unittest.c
@@ -47,28 +47,9 @@ test_devinfo_repository__load_devinfo_from_file(Moat in_moat)
 {
   TDEVINFORepository repo;
   SSEString* path;
-
-  // Intialize
-  ASSERT(TDEVINFORepository_Initialize(&repo, in_moat) == SSE_E_OK);
-
-  // Load devinfo from the file.
-  ASSERT((path = sse_string_new("./devinfo.json")) != NULL);
-  ASSERT(TDEVINFORepository_LoadDevinfo(&repo, path) == SSE_E_OK);
-
-  // Cleanup
-  TDEVINFORepository_Finalize(&repo);
-  sse_string_free(path, sse_true);
-
-  return TEST_RESULT_UNKNOWN;
-}
-
-static sse_int
-test_devinfo_repository__get_all_with_json(Moat in_moat)
-{
-  TDEVINFORepository repo;
-  SSEString* path;
   SSEString* json = NULL;
   sse_char* json_cstr;
+  sse_char* expected = "{\"hardware\":{\"platform\":{\"vendor\":\"Inventit\",\"product\":\"IVI IoT Gateway\",\"model\":\"IVI-0001\",\"serial\":\"123456\",\"hwVersion\":\"1.0.0\",\"fwVersion\":\"2.0.1\",\"deviceId\":\"IVI:SN:123456\",\"category\":\"gateway\"},\"modem\":{\"type\":\"XXX\",\"hwVersion\":\"rev1.2.0\",\"fwVersion\":\"v2.0.0\"},\"network\":{\"interface\":[{\"name\":\"eth0\",\"hwAddress\":\"00:11:22:33:44:55\",\"ipv4Address\":\"192.168.1.25\",\"netmask\":\"255.255.255.0\",\"ipv6Address\":\"fe80::a00:27ff:feaa:38a9/64\"},{\"name\":\"usb0\",\"hwAddress\":\"EE:07:72:2F:01:07\",\"ipv4Address\":\"153.158.37.30\",\"netmask\":\"255.255.255.255\",\"ipv6Address\":\"fe80::ec07:72ff:fe2f:107/64\"}],\"nameserver\":[\"222.146.35.1\",\"221.184.25.1\"]},\"sim\":[{\"iccid\":\"898110011111111111111\",\"imsi\":\"121234561234560\",\"msisdn\":\"09012345678\"}]},\"software\":{\"os\":{\"type\":\"Linux\",\"version\":\"2.6.26\"},\"sscl\":{\"type\":\"SSEGW\",\"version\":\"1.0.6\",\"sdkVersion\":\"1.0.5\"}}}";
 
   // Initialize
   ASSERT(TDEVINFORepository_Initialize(&repo, in_moat) == SSE_E_OK);
@@ -80,7 +61,9 @@ test_devinfo_repository__get_all_with_json(Moat in_moat)
   // Seriallize with JSON
   ASSERT(TDEVINFORepository_GetDevinfoWithJson(&repo, NULL, &json) == SSE_E_OK);
   ASSERT((json_cstr = sse_strndup(sse_string_get_cstr(json), sse_string_get_length(json))) != NULL);
-  LOG_PRINT("Devinfo = [%s]", json_cstr);
+  LOG_PRINT("Actual   = [%s]\n", json_cstr);
+  LOG_PRINT("Expected = [%s]\n", expected);
+  ASSERT(sse_strcmp(json_cstr, expected) == 0);
 
   // Cleanup
   TDEVINFORepository_Finalize(&repo);
@@ -88,7 +71,7 @@ test_devinfo_repository__get_all_with_json(Moat in_moat)
   sse_string_free(json, sse_true);
   sse_free(json_cstr);
 
-  return TEST_RESULT_UNKNOWN;
+  return TEST_RESULT_SUCCESS;
 }
 
 static sse_int
@@ -221,6 +204,189 @@ test_devinfo_repository__add_interfaces(Moat in_moat)
   return TEST_RESULT_SUCCESS;
 }
 
+static sse_int
+test_devinfo_repository__add_all_items(Moat in_moat)
+{
+  TDEVINFORepository repo;
+  SSEString* item;
+  SSEString* json = NULL;
+  sse_char* json_cstr;
+
+  // Initialize
+  ASSERT(TDEVINFORepository_Initialize(&repo, in_moat) == SSE_E_OK);
+
+  // Add all items
+  { // Hardware
+    { // Platform
+      // Vendor
+      ASSERT((item = sse_string_new("Raspberry Pi Fundation")) != NULL);
+      ASSERT(TDEVINFORepository_SetHardwarePlatformVendor(&repo, item) == SSE_E_OK);
+      sse_string_free(item, sse_true);
+
+      // Product
+      ASSERT((item = sse_string_new("Raspberry Pi")) != NULL);
+      ASSERT(TDEVINFORepository_SetHardwarePlatformProduct(&repo, item) == SSE_E_OK);
+      sse_string_free(item, sse_true);
+
+      // Model
+      ASSERT((item = sse_string_new("RASPBERRY PI 1 MODEL B+")) != NULL);
+      ASSERT(TDEVINFORepository_SetHardwarePlatformModel(&repo, item) == SSE_E_OK);
+      sse_string_free(item, sse_true);
+
+      // Serial No.
+      ASSERT((item = sse_string_new("ABC12345")) != NULL);
+      ASSERT(TDEVINFORepository_SetHardwarePlatformSerial(&repo, item) == SSE_E_OK);
+      sse_string_free(item, sse_true);
+
+      // Hardware version
+      ASSERT((item = sse_string_new("1.2.3")) != NULL);
+      ASSERT(TDEVINFORepository_SetHardwarePlatformHwVersion(&repo, item) == SSE_E_OK);
+      sse_string_free(item, sse_true);
+
+      // Firmware versionn
+      ASSERT((item = sse_string_new("2.4.5")) != NULL);
+      ASSERT(TDEVINFORepository_SetHardwarePlatformFwVersion(&repo, item) == SSE_E_OK);
+      sse_string_free(item, sse_true);
+
+      // Device Id
+      ASSERT((item = sse_string_new("RSP:SN:123456")) != NULL);
+      ASSERT(TDEVINFORepository_SetHardwarePlatformDeviceId(&repo, item) == SSE_E_OK);
+      sse_string_free(item, sse_true);
+
+      // Category
+      ASSERT((item = sse_string_new("gateway")) != NULL);
+      ASSERT(TDEVINFORepository_SetHardwarePlatformCategory(&repo, item) == SSE_E_OK);
+      sse_string_free(item, sse_true);
+    }
+    { // Modem
+      // Type
+      ASSERT((item = sse_string_new("MC8090")) != NULL);
+      ASSERT(TDEVINFORepository_SetHardwareModemType(&repo, item) == SSE_E_OK);
+      sse_string_free(item, sse_true);
+
+      // Hardware version
+      ASSERT((item = sse_string_new("1.2.3")) != NULL);
+      ASSERT(TDEVINFORepository_SetHardwareModemHwVersion(&repo, item) == SSE_E_OK);
+      sse_string_free(item, sse_true);
+
+      // Frimware version
+      ASSERT((item = sse_string_new("4.5.6")) != NULL);
+      ASSERT(TDEVINFORepository_SetHardwareModemFwVersion(&repo, item) == SSE_E_OK);
+      sse_string_free(item, sse_true);
+    }
+    { // Network
+      { // Interface
+	// eth0
+	SSEString* if_name;
+	SSEString* if_macaddr;
+	SSEString* if_ipv4addr;
+	SSEString* if_netmask;
+	SSEString* if_ipv6addr;
+
+	ASSERT((if_name     = sse_string_new("eth0")));
+	ASSERT((if_macaddr  = sse_string_new("00:11:22:aa:bb:cc")));
+	ASSERT((if_ipv4addr = sse_string_new("192.128.1.32")));
+	ASSERT((if_netmask  = sse_string_new("255.255.255.0")));
+	ASSERT((if_ipv6addr = sse_string_new("fe80::a00:27ff:fea9:d684/64")));
+	ASSERT(TDEVINFORepository_AddHadwareNetworkInterface(&repo, if_name, if_macaddr, if_ipv4addr, if_netmask, if_ipv6addr) == SSE_E_OK);
+	sse_string_free(if_name, sse_true);
+	sse_string_free(if_macaddr, sse_true);
+	sse_string_free(if_ipv4addr, sse_true);
+	sse_string_free(if_netmask, sse_true);
+	sse_string_free(if_ipv6addr, sse_true);
+
+	// wlan0
+	ASSERT((if_name     = sse_string_new("wlan0")));
+	ASSERT((if_macaddr  = sse_string_new("88:1f:a1:10:fd:c8")));
+	ASSERT((if_ipv4addr = sse_string_new("192.168.1.11")));
+	ASSERT((if_netmask  = sse_string_new("255.255.255.0")));
+	ASSERT((if_ipv6addr = sse_string_new("fe80::8a1f:a1ff:fe10:fdc8/64")));
+	ASSERT(TDEVINFORepository_AddHadwareNetworkInterface(&repo, if_name, if_macaddr, if_ipv4addr, if_netmask, if_ipv6addr) == SSE_E_OK);
+	sse_string_free(if_name, sse_true);
+	sse_string_free(if_macaddr, sse_true);
+	sse_string_free(if_ipv4addr, sse_true);
+	sse_string_free(if_netmask, sse_true);
+	sse_string_free(if_ipv6addr, sse_true);
+      }
+      { // Nameserver
+#if 0 // Not implemented yet
+	// Server 1
+	ASSERT((item = sse_string_new("222.146.35.1")) != NULL);
+	ASSERT(TDEVINFORepository_AddHardwareNetworkNameserver(&repo, item) == SSE_E_OK);
+	sse_string_free(item, sse_true);
+
+	// Server 2
+	ASSERT((item = sse_string_new("221.184.25.1")) != NULL);
+	ASSERT(TDEVINFORepository_AddHardwareNetworkNameserver(&repo, item) == SSE_E_OK);
+	sse_string_free(item, sse_true);
+#endif
+      }
+    }
+    { // SIM
+#if 0 // Not implemented yet
+      SSEString* iccid;
+      SSEString* imsi;
+      SSEString* msisdn;
+
+      // SIM 1
+      ASSERT((iccid = sse_string_new("898110011111111111111")));
+      ASSERT((imsi = sse_string_new("121234561234560")));
+      ASSERT((msisdn = sse_string_new("09012345678")));
+      ASSERT(TDEVINFORepository_AddHardwareSim(&repo, iccid, imsi, msisdn) == SSE_E_OK);
+      sse_string_free(iccid, sse_true);
+      sse_string_free(imsi, sse_true);
+      sse_string_free(msisdn, sse_true);
+
+      // SIM 2
+      ASSERT((iccid = sse_string_new("000000000000000000000")));
+      ASSERT((imsi = sse_string_new("111111111111111")));
+      ASSERT((msisdn = sse_string_new("22222222222")));
+      ASSERT(TDEVINFORepository_AddHardwareSim(&repo, iccid, imsi, msisdn) == SSE_E_OK);
+      sse_string_free(iccid, sse_true);
+      sse_string_free(imsi, sse_true);
+      sse_string_free(msisdn, sse_true);
+#endif
+    }
+    { // Software
+      { // OS
+	SSEString* type;
+	SSEString* version;
+
+	ASSERT((type = sse_string_new("Linux")) != NULL);
+	ASSERT((version = sse_string_new("2.6.26")) != NULL);
+	ASSERT(TDEVINFORepository_SetSoftwareOS(&repo, type, version) == SSE_E_OK);
+	sse_string_free(type, sse_true);
+	sse_string_free(version, sse_true);
+      }
+      { // ServiceSync Client
+	SSEString* type;
+	SSEString* version;
+	SSEString* sdk_version;
+
+	ASSERT((type = sse_string_new("SSEGW")) != NULL);
+	ASSERT((version = sse_string_new("1.0.7")) != NULL);
+	ASSERT((sdk_version = sse_string_new("1.0.5")) != NULL);
+	ASSERT(TDEVINFORepository_SetSoftwareSscl(&repo, type, version, sdk_version) == SSE_E_OK);
+	sse_string_free(type, sse_true);
+	sse_string_free(version, sse_true);
+	sse_string_free(sdk_version, sse_true);
+      }
+    }
+  }
+
+  // Get devinfo with JSON
+  ASSERT(TDEVINFORepository_GetDevinfoWithJson(&repo, NULL, &json) == SSE_E_OK);
+  ASSERT((json_cstr = sse_strndup(sse_string_get_cstr(json), sse_string_get_length(json))) != NULL);
+  LOG_PRINT("Actual = [%s]", json_cstr);
+
+  // Cleanup
+  TDEVINFORepository_Finalize(&repo);
+  sse_string_free(json, sse_true);
+  sse_free(json_cstr);
+
+  return TEST_RESULT_SUCCESS;
+}
+
 static void
 test_report(void)
 {
@@ -239,10 +405,11 @@ moat_app_main(sse_int in_argc, sse_char *argv[])
 	}
 
 	DO_TEST(test_devinfo_repository__load_devinfo_from_file(moat));
-	DO_TEST(test_devinfo_repository__get_all_with_json(moat));
 	DO_TEST(test_devinfo_repository__add_vendor(moat));
 	DO_TEST(test_devinfo_repository__overwrite_vendor(moat));
 	DO_TEST(test_devinfo_repository__add_interfaces(moat));
+	DO_TEST(test_devinfo_repository__add_all_items(moat));
+
 	test_report();
 
 	moat_destroy(moat);
